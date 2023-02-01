618f9041b854238b51cf0b2fa6286e24a909d7c8
rabbi mathake <mathakerabbi@hotmail.com>
2023-02-02 01:53:41 +0200

    Added new test case "ExampleTest1" for questionone. Added validation for the words in GetLongestSetence method

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Data.Common;
 
 namespace Inoxico.TechnicalQuestions.Answers
@@ -15,29 +16,61 @@ namespace Inoxico.TechnicalQuestions.Answers
          
             for (int i = 0; i < dot.Length; i++)
             {
-                //Split setence if contains ?
+                //Split setence if contains ? then split words
                 if (dot[i].Contains("?"))
                 {
                     string[] q = s.Split('?');
                     for (int qu = 0; qu < q.Length; qu++)
                     {
                         string[] words = dot[qu].Split(' ');
+
+                        //Check if word is valid word else remove
+                        for(int wd = 0; wd < words.Length; wd++)
+                        {
+                            if (!words[wd].All(Char.IsLetter))
+                            {
+                                List<string> pos = new List<string>(words);
+                                pos.RemoveAt(wd);
+                                words = pos.ToArray();
+                            }
+                        }
                         sentenceLength.Add(words.Length);
                     }
                 }
-                //Split setence if contains !
+                //Split setence if contains ! then split words
                 else if (dot[i].Contains("!"))
                 {
                     string[] ex = s.Split('!');
                     for (int e = 0; e < ex.Length;e++)
                     {
                         string[] words = dot[e].Split(' ');
+                        //Check if word is valid word else remove
+                        for (int wd = 0; wd < words.Length; wd++)
+                        {
+                            if (!words[wd].All(Char.IsLetter))
+                            {
+                                List<string> pos = new List<string>(words);
+                                pos.RemoveAt(wd);
+                                words = pos.ToArray();
+                            }
+                        }
                         sentenceLength.Add(words.Length);
                     }
                 }
                 else
                 {
+                    //then split words
                     string[] words = dot[i].Split(' ');
+                    //Check if word is valid word else remove
+                    for (int wd = 0; wd < words.Length; wd++)
+                    {
+                        if (!words[wd].All(Char.IsLetter))
+                        {
+                            List<string> pos = new List<string>(words);
+                            pos.RemoveAt(wd);
+                            words = pos.ToArray();
+                        }
+                    }
                     sentenceLength.Add(words.Length);
                 }
             }
--- Inoxico.TechnicalQuestions.Tests/QuestionOneTests.cs
+++ Inoxico.TechnicalQuestions.Tests/QuestionOneTests.cs
@@ -13,5 +13,14 @@ namespace Inoxico.TechnicalQuestions.Tests
 
             Assert.Equal(4, outcome);
         }
+
+        [Fact]
+        public void ExampleTest1()
+        {
+            var input = $"Forget CVs..Save time . x x”";
+            var outcome = QuestionOne.GetLongestSentance(input);
+
+            Assert.Equal(2, outcome);
+        }
     }
 }
