872f9fba3b31b1b28bdddd62055d389d70cf61f1
rabbi mathake <mathakerabbi@hotmail.com>
2023-02-02 02:19:04 +0200

    Added if statement to validate pitdepth and return pitdepth in GetPitDepth Method.

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,7 +5,23 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            throw new NotImplementedException();
+            //Declare variable
+            int pitdepth = 0;
+            int p, q, r;
+
+            for(int i  = 0; i < points.Length; i++)
+            {
+                p = i;
+                q = i + 1;
+                r = i + 2;
+
+                //Check depth condition and return pitdepth
+                if ((points[p] >= 0) && (points[p] > points[q]) && (points[q] < points[r])){
+                    pitdepth = Math.Min();
+                }
+            }
+
+            return pitdepth;
         }
     }
 }
