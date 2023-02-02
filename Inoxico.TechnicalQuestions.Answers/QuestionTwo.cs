fd5e373a3980c3a1ff16348772e3e7d2fd766f9a
rabbi mathake <mathakerabbi@hotmail.com>
2023-02-02 02:47:11 +0200

    Added an else condition on the GetPitDepth function if statement

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -20,7 +20,7 @@ namespace Inoxico.TechnicalQuestions.Answers
                //Check depth condition and return pitdepth
                 if (r <= points.Length)
                 {
-                    if ((points[p] >= 0) && (points[p] > points[q]) && (points[q] < points[r]))
+                    if ((points[p] > 0) && (points[p] > points[q]) && (points[q] < points[r]))
                     {
                         deepestPit.Add(Math.Min(points[p] - points[q], points[r] - points[q]));
                     }
